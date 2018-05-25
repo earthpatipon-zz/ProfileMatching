24c33a75112dbcdda6342fee03307f11546dbc2e
earthpatipon <eartyearth@gmail.com>
2018-05-26 00:46:29 +0700

    so lazyy

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd
-import collections
 import math
+import collections
+import pandas as pd
 import numpy as np
-from textblob import TextBlob as tb
+import textblob as tb
 
 
 # import data
@@ -18,109 +18,118 @@ relatedList = []    # list of people related to keywords
 docList = data['Title']
 abstractList = data['Abstract']
 citedList = data['Cited by']
-authorList = data['Authors with affiliations'].str.split(';')       # yield list ['name., address'] , ['name., address'], .....
-authorKeywordList = data['Author Keywords'].str.split(';')          # yield list ['key1', 'key2', 'key3', ...., 'keyN']
-indexKeywordList = data['Index Keywords'].str.split(';')            # yield list ['key1', 'key2', 'key3', ...., 'keyN']
+authorList = data['Authors with affiliations'].str.split('; ')       # yield list ['name., address'] , ['name., address'], .....
+authorKeywordList = data['Author Keywords'].str.split('; ')          # yield list ['key1', 'key2', 'key3', ...., 'keyN']
+indexKeywordList = data['Index Keywords'].str.split('; ')            # yield list ['key1', 'key2', 'key3', ...., 'keyN']
 
+# for i in range(len(authorKeywordList)):
+#     if math.isnan(authorKeywordList[i]):
+#         print("NaN")
 
-# print (absArray)
-
+# print(authorKeywordList)
+# print(indexKeywordList)
 for i in range(len(data)):
-    keyword = authorKeywordList[i] if not(authorKeywordList[i]) else ''
     document = docList[i]
+    if math.isnan(authorKeywordList[i]):
+        print("hi")
+    authorKeyword = authorKeywordList[i] if authorKeywordList[i] != np.nan else []
+    indexKeyword = indexKeywordList[i] if indexKeywordList[i] != np.nan else []
 
     for j in range(len(authorList[i])):
         authorAffiliation = authorList[i][j].split('.,')   # yield list ['name', 'address'] which index is [0,1]
         author = authorAffiliation[0]
-        affiliation = authorAffiliation[1] if len(authorAffiliation) > 1 else ''
+        affiliation = authorAffiliation[1] if len(authorAffiliation) > 1 else []
 
         if author not in dic:
-            dic[author] = {'Author': author, 'Affiliation': affiliation, 'Document': document, 'Keyword': keyword}
-            # print dic[author]
+            dic[author] = {'Author': author, 'Affiliation': affiliation, 'Document': document, 'AuthorKeyword': authorKeyword, 'IndexKeyword': indexKeyword}
 
         else:
-            add = dic[author]['Affiliation']
-            doc = dic[author]['Document']
-            key = dic[author]['Keyword']
+            if not dic[author]['Affiliation']:
+                dic[author]['Affiliation'] = affiliation
+            dic[author]['Document'] += document
+            dic[author]['AuthorKeyword'] += authorKeyword
+            dic[author]['IndexKeyword'] += indexKeyword
 
-            dic[author]['Document'] = doc + document
-            # print "####################################"
-            # print dic[author]
+            # print("####################################")
+            # print(dic[author]['Document'])
+            # print(dic[author]['AuthorKeyword'])
+            # print(dic[author]['IndexKeyword'])
             # print type(dic['Address'])
             # print type(dic['Keyword'])
             # print type(dic['Document'])
 
-# take input
-# query = raw_input("Keywords to find list of people related to: ")
-query = input("Keywords to find list of people related to: ")
-query = query.split(',')
 
-for k, v in dic.items():
-    for x in query:
-        if x in v['Keyword']:
-            relatedList.append({k:v})
-            continue;
+# # take input
+# # query = raw_input("Keywords to find list of people related to: ")
+# query = input("Keywords to find list of people related to: ")
+# query = query.split(',')
+#
+# for k, v in dic.items():
+#     for x in query:
+#         if x in v['IndexKeyword']:
+#             relatedList.append({k:v})
+#             continue;
+#
+#
+# for i in relatedList:
+#     # print i.values()
+#     for k in i:
+#         print(i[k]['Keyword'])
 
 
-for i in relatedList:
-    # print i.values()
-    for k in i:
-        print (i[k]['Keyword'])
-
-
-# td-idf (term frequency and inverse document frequency)
-def tf(word, blob):
-    return blob.words.count(word) / len(blob.words)
-
-def n_containing(word, bloblist):
-    return sum(1 for blob in bloblist if word in blob.words)
-
-def idf(word, bloblist):
-    return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
-
-def tfidf(word, blob, bloblist):
-    return tf(word, blob) * idf(word, bloblist)
-
-document1 = tb("""Python is a 2000 made-for-TV horror movie directed by Richard
-Clabaugh. The film features several cult favorite actors, including William
-Zabka of The Karate Kid fame, Wil Wheaton, Casper Van Dien, Jenny McCarthy,
-Keith Coogan, Robert Englund (best known for his role as Freddy Krueger in the
-A Nightmare on Elm Street series of films), Dana Barron, David Bowe, and Sean
-Whalen. The film concerns a genetically engineered snake, a python, that
-escapes and unleashes itself on a small town. It includes the classic final
-girl scenario evident in films like Friday the 13th. It was filmed in Los Angeles,
- California and Malibu, California. Python was followed by two sequels: Python
- II (2002) and Boa vs. Python (2004), both also made-for-TV films.""")
-
-document2 = tb("""Python, from the Greek word (πύθων/πύθωνας), is a genus of
-nonvenomous pythons[2] found in Africa and Asia. Currently, 7 species are
-recognised.[2] A member of this genus, P. reticulatus, is among the longest
-snakes known.""")
-
-document3 = tb("""The Colt Python is a .357 Magnum caliber revolver formerly
-manufactured by Colt's Manufacturing Company of Hartford, Connecticut.
-It is sometimes referred to as a "Combat Magnum".[1] It was first introduced
-in 1955, the same year as Smith &amp; Wesson's M29 .44 Magnum. The now discontinued
-Colt Python targeted the premium revolver market segment. Some firearm
-collectors and writers such as Jeff Cooper, Ian V. Hogg, Chuck Hawks, Leroy
-Thompson, Renee Smeets and Martin Dougherty have described the Python as the
-finest production revolver ever made.""")
-
-abstractList = [document1,document2,document3]
-
-for i, blob in enumerate(abstractList):
-    print("Top words in document {}".format(i + 1))
-    scores = {word: tfidf(word, blob, abstractList) for word in blob.words}
-    sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-    for word, score in sorted_words[:3]:
-        print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
-        #         print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
-        if add == '':
-            dic[author]['Address'] = add + affiliation
-        if key == '':
-            dic[author]['Keyword'] = keyword
-        else:
-            dic[author]['Keyword'] = key + keyword
-
-for i in dic:
-    print i[1]
+# # td-idf (term frequency and inverse document frequency)
+# def tf(word, blob):
+#     return blob.words.count(word) / len(blob.words)
+#
+# def n_containing(word, bloblist):
+#     return sum(1 for blob in bloblist if word in blob.words)
+#
+# def idf(word, bloblist):
+#     return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
+#
+# def tfidf(word, blob, bloblist):
+#     return tf(word, blob) * idf(word, bloblist)
+#
+# document1 = tb("""Python is a 2000 made-for-TV horror movie directed by Richard
+# Clabaugh. The film features several cult favorite actors, including William
+# Zabka of The Karate Kid fame, Wil Wheaton, Casper Van Dien, Jenny McCarthy,
+# Keith Coogan, Robert Englund (best known for his role as Freddy Krueger in the
+# A Nightmare on Elm Street series of films), Dana Barron, David Bowe, and Sean
+# Whalen. The film concerns a genetically engineered snake, a python, that
+# escapes and unleashes itself on a small town. It includes the classic final
+# girl scenario evident in films like Friday the 13th. It was filmed in Los Angeles,
+#  California and Malibu, California. Python was followed by two sequels: Python
+#  II (2002) and Boa vs. Python (2004), both also made-for-TV films.""")
+#
+# document2 = tb("""Python, from the Greek word (πύθων/πύθωνας), is a genus of
+# nonvenomous pythons[2] found in Africa and Asia. Currently, 7 species are
+# recognised.[2] A member of this genus, P. reticulatus, is among the longest
+# snakes known.""")
+#
+# document3 = tb("""The Colt Python is a .357 Magnum caliber revolver formerly
+# manufactured by Colt's Manufacturing Company of Hartford, Connecticut.
+# It is sometimes referred to as a "Combat Magnum".[1] It was first introduced
+# in 1955, the same year as Smith &amp; Wesson's M29 .44 Magnum. The now discontinued
+# Colt Python targeted the premium revolver market segment. Some firearm
+# collectors and writers such as Jeff Cooper, Ian V. Hogg, Chuck Hawks, Leroy
+# Thompson, Renee Smeets and Martin Dougherty have described the Python as the
+# finest production revolver ever made.""")
+#
+# abstractList = [document1,document2,document3]
+#
+# for i, blob in enumerate(abstractList):
+#     print("Top words in document {}".format(i + 1))
+#     scores = {word: tfidf(word, blob, abstractList) for word in blob.words}
+#     sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+#     for word, score in sorted_words[:3]:
+#         print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
+#         #         print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
+#         if add == '':
+#             dic[author]['Address'] = add + affiliation
+#         if key == '':
+#             dic[author]['Keyword'] = keyword
+#         else:
+#             dic[author]['Keyword'] = key + keyword
+#
+# for i in dic:
+#     print(i[1])
